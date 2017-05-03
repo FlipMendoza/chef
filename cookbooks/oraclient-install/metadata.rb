315893262cff2a58587cf0f94590ca2bce3349de
FlipMendoza <luis.felipex.mendoza.orozco@intel.com>
2017-05-02 17:09:28 -0700

    name convention
    changed and adapted a name convention on oracle-client

--- cookbooks/oraclient-install/metadata.rb
+++ cookbooks/oraclient-install/metadata.rb
@@ -2,7 +2,7 @@ name             'oraclient-install'
 maintainer       'Softtek'
 maintainer_email 'luis.mendoza@softtek.com'
 license          'All rights reserved'
-description      'Installs/Configures oracle sql client'
+description      'Installs/Configures an oracle SQL client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 issues_url 'https://github.com/FlipMendoza/chef/issues' if respond_to?(:issues_url)
