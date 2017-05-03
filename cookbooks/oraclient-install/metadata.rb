8d1fb54015ecd3bddfaba6a0b89dd40996995925
FlipMendoza <luis.felipex.mendoza.orozco@intel.com>
2017-05-03 13:02:04 -0700

    Correction made to oraclient-install cookbook
    First test on this cookbook meant to pass by

--- cookbooks/oraclient-install/metadata.rb
+++ cookbooks/oraclient-install/metadata.rb
@@ -8,4 +8,4 @@ version          '0.1.0'
 issues_url 'https://github.com/FlipMendoza/chef/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/FlipMendoza/chef' if respond_to?(:source_url)
 chef_version '>= 11.1.3' if respond_to?(:chef_version)
-#supports 'redhat'
+supports 'redhat'
