2289c0299620caa6dd90f33ff8218692a701dbc9
FlipMendoza <luis.felipex.mendoza.orozco@intel.com>
2017-05-11 22:20:35 -0700

    Now this is real
    Change done due pre-demo presentation

--- cookbooks/oraclient-install/metadata.rb
+++ cookbooks/oraclient-install/metadata.rb
@@ -5,7 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures an oracle SQL client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-#issues_url 'https://github.com/FlipMendoza/chef/issues' if respond_to?(:issues_url)
+issues_url 'https://github.com/FlipMendoza/chef/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/FlipMendoza/chef' if respond_to?(:source_url)
-#chef_version '>= 11.1.3' if respond_to?(:chef_version)
+chef_version '>= 11.1.3' if respond_to?(:chef_version)
 supports 'redhat'
